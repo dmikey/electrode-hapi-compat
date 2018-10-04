8d92ca478747d1419c4be6bbfee42c5e6d487e57
Dat Vong <Dat.Vong@walmartlabs.com>
2018-10-03 20:55:25 -0700

    Support for converting Plugin to Hapi 16

--- test/spec/util.spec.js
+++ test/spec/util.spec.js
@@ -1,9 +1,12 @@
 "use strict";
 
+const hapi = require("hapi");
 const mockRequire = require("mock-require");
 
 describe("Util", () => {
   let index;
+  let hapiServer;
+  let extCalled;
 
   const hapi17Plugin = {
     register: () => {},
@@ -12,8 +15,34 @@ describe("Util", () => {
       version: "1.0.0"
     }
   };
+  const hapi17PluginWithExt = {
+    register: function(server) {
+      server.ext("onPreHandler", function(request, h) {
+        extCalled.onPreHandler = true;
+        return h.continue;
+      });
+    },
+    pkg: {
+      name: "PluginForExt",
+      version: "1.0.1"
+    }
+  };
+
+  beforeEach(() => {
+    extCalled = [];
+    hapiServer = new hapi.Server();
+    hapiServer.connection({ port: 80 });
+    hapiServer.route({
+      method: "GET",
+      path: "/test",
+      handler: (req, reply) => {
+        return reply("ok");
+      }
+    });
+  });
 
   afterEach(() => {
+    hapiServer.stop();
     delete require.cache[require.resolve("../../lib/util")];
     mockRequire.stopAll();
   });
@@ -24,40 +53,41 @@ describe("Util", () => {
   });
 
   it("test is hapi 17", () => {
-    mockRequire("hapi/package", { version: "16.2.1" });
-    index = require("../../lib/util");
-    expect(index.isHapi17).false;
-  });
-
-  it("test is not hapi 17", () => {
     mockRequire("hapi/package", { version: "17.2.2" });
     index = require("../../lib/util");
     expect(index.isHapi17).true;
   });
 
-  it("test no hapi", () => {
+  it("test is not hapi 17", () => {
+    index = require("../../lib/util");
+    expect(index.isHapi17).false;
+  });
+
+  it("test no hapi defaults hapi 16", () => {
     mockRequire("hapi/package", null);
     index = require("../../lib/util");
     expect(index.isHapi17).false;
   });
 
-  it("test supportHapi16 returns Hapi17 plugin for Hapi17", () => {
+  it("test supportHapi16 handles Hapi17 plugin on Hapi 17", () => {
     mockRequire("hapi/package", { version: "17.0.0" });
     index = require("../../lib/util");
     const plugin = index.supportHapi16(hapi17Plugin);
     expect(plugin).equal(hapi17Plugin);
   });
 
-  it("test supportHapi16 return Hapi16 plugin on Hapi16", () => {
-    mockRequire("hapi/package", { version: "16.0.0" });
+  it("test supportHapi16 converts Hapi17 plugin with ext", done => {
     index = require("../../lib/util");
-    const plugin = index.supportHapi16(hapi17Plugin);
-    expect(plugin).a("function");
-    expect(plugin.attributes.pkg).equal(hapi17Plugin.pkg);
-    let nextCalled = false;
-    plugin({}, {}, () => {
-      nextCalled = true;
+    const plugin16 = index.supportHapi16(hapi17PluginWithExt);
+
+    hapiServer.register(plugin16);
+
+    hapiServer.inject("/test", res => {
+      expect(plugin16).a("function");
+      expect(plugin16.attributes.pkg).equal(hapi17PluginWithExt.pkg);
+      expect(extCalled.onPreHandler).true;
+      expect(res.result).eq("ok");
+      done();
     });
-    expect(nextCalled).true;
   });
 });
